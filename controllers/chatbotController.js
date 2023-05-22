1be1fb6d81bc564e523e92b31e25b2d7d33ce8c5
Evan Cheng <evancheng2014@gmail.com>
2023-05-21 21:02:00 -0700

    remove unused files

--- controllers/chatbotController.js
+++
@@ -1,3 +0,0 @@
-export const getResponse = async (req, res) => {
-    
-}
--- routes/chatbotRoute.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-const chatbotController = require("../controllers/chatbotController");
-
-router.route("/").post(chatbotController.getResponse);
-module.exports = router;
